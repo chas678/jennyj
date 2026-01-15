b7b33c2cbb67dd1bc10443d56bb0eda7fcd7de53
Christopher Barham <Christopher.Barham@nordnet.se>
2026-01-15 10:13:35 +0100

    reduce time

--- src/main/java/com/pobox/chas66/PairwiseSolverFactory.java
+++ src/main/java/com/pobox/chas66/PairwiseSolverFactory.java
@@ -1,10 +1,8 @@
 package com.pobox.chas66;
 
 import ai.timefold.solver.core.config.heuristic.selector.entity.EntitySelectorConfig;
-import ai.timefold.solver.core.config.heuristic.selector.entity.pillar.PillarSelectorConfig;
 import ai.timefold.solver.core.config.heuristic.selector.move.composite.UnionMoveSelectorConfig;
 import ai.timefold.solver.core.config.heuristic.selector.move.generic.ChangeMoveSelectorConfig;
-import ai.timefold.solver.core.config.heuristic.selector.move.generic.PillarSwapMoveSelectorConfig;
 import ai.timefold.solver.core.config.localsearch.LocalSearchPhaseConfig;
 import ai.timefold.solver.core.config.localsearch.decider.acceptor.LocalSearchAcceptorConfig;
 import ai.timefold.solver.core.config.solver.SolverConfig;
@@ -20,7 +18,7 @@ public class PairwiseSolverFactory {
                 .withConstraintProviderClass(PairwiseConstraintProvider.class)
                 .withTerminationConfig(new TerminationConfig()
                         .withUnimprovedSecondsSpentLimit(10L)
-                        .withSecondsSpentLimit(60L))
+                        .withSecondsSpentLimit(30L))
                 .withPhases(
                         new LocalSearchPhaseConfig()
                                 .withAcceptorConfig(new LocalSearchAcceptorConfig()
